0c790efad900adb9bbbc5c64097706f4f302b861
Anton1824 <152066703+Anton1824@users.noreply.github.com>
2024-05-19 19:58:53 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import { nanoid } from "nanoid";
 
 const data = [
   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-2", name: "Herm Kline", number: "443-89-12" },
   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
 ];
@@ -40,7 +40,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Phonebook</h1>
+      <h1>Phone</h1>
       <ContactForm handleSubmit={handleSubmit} />
       <SearchBox setFiltered={setFiltered} />
       <ContactList contacts={newData} handleDelete={handleDelete} />
